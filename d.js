e8730512a6a41a2d3b2826bbbf6b48298665790b
mashitu <624952546@qq.com>
2022-01-05 14:49:55 +0800

    Revert "12222"
    This reverts commit 3c752a4063118310096cdc6b3eafcd396453eeee.

--- d.js
+++ d.js
@@ -1,10 +1,10 @@
 /*
  * @Author: mashitu
  * @Date: 2022-01-05 10:13:15
- * @LastEditTime: 2022-01-05 14:33:00
+ * @LastEditTime: 2022-01-05 13:49:34
  * @LastEditors: mashitu
  * @Description: 分支ceshi to bo commit3
  */
 function d() {
-  console.log(12222)
+  console.log(1)
 }
