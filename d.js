9ce8a4cde6b3e200e3c23e72cc1a5bed0fc0617d
mashitu <624952546@qq.com>
2022-01-05 13:49:42 +0800

    sf

--- d.js
+++ d.js
@@ -1,7 +1,10 @@
 /*
  * @Author: mashitu
  * @Date: 2022-01-05 10:13:15
- * @LastEditTime: 2022-01-05 10:13:15
+ * @LastEditTime: 2022-01-05 13:49:34
  * @LastEditors: mashitu
  * @Description: 分支ceshi to bo commit3
  */
+function d() {
+  console.log(1)
+}
