9d8ee1f1fd81aea50a3256559d53312c0e892a6b
mashitu <624952546@qq.com>
2022-01-05 10:03:49 +0800

    分支commit1

--- a.js
+++ a.js
@@ -1,7 +1,11 @@
 /*
  * @Author: mashitu
  * @Date: 2022-01-05 09:59:52
- * @LastEditTime: 2022-01-05 09:59:53
+ * @LastEditTime: 2022-01-05 10:01:52
  * @LastEditors: mashitu
  * @Description: git 测试
  */
+function a() {
+  console.log('a')
+}
+a()
