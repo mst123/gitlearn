9e77e24e93ecd819a99e7e3bcce536371af77ef2
mashitu <624952546@qq.com>
2022-01-06 09:38:49 +0800

    haha

--- c.js
+++ c.js
@@ -1,10 +1,18 @@
 /*
  * @Author: mashitu
  * @Date: 2022-01-05 10:12:15
- * @LastEditTime: 2022-01-05 15:08:03
+ * @LastEditTime: 2022-01-06 09:36:07
  * @LastEditors: mashitu
  * @Description: ceshi commit2
  */
 function ccc() {
   
+}
+
+function name() {
+  console.log("checkout测试");
+}
+
+function s() {
+  console.log("没暂存")
 }
